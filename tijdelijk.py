1f1992623215f90c134394d7bf9977a56a8a23d2
james van Veen <jamesvanveen@live.com>
2026-04-17 16:40:09 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,26 @@
+mijn_dictionary = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5,
+}
+aanbieding = 3*0.8
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split(" ")
+for el in reclame_tekst4:
+    if len(el)>= 5:
+        print(el.upper())
+    elif len(el) <= 4:
+        print(el.lower())
+print(reclame_tekst4)
+
+
+
+
+
+
+
+
+
+
